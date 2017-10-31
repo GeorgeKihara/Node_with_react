f9117ac37c63002f3d2283ea1dcf1101c34c37c9
GeorgeKihara <gegohcomp@gmail.com>
2017-11-01 01:31:47 +0300

    footer component

---
+++ app/components/Footer.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Link} from 'react-router';
+import FooterStore from '../stores/FooterStore'
+import FooterActions from '../actions/FooterActions';
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = FooterStore.getState();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    FooterStore.listen(this.onChange);
+    FooterActions.getTopCharacters();
+  }
+
+  componentWillUnmount() {
+    FooterStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
+  }
+
+  render() {
+    let leaderboardCharacters = this.state.characters.map((character) => {
+      return (
+        <li key={character.characterId}>
+          <Link to={'/characters/' + character.characterId}>
+            <img className='thumb-md' src={'http://image.eveonline.com/Character/' + character.characterId + '_128.jpg'} />
+          </Link>
+        </li>
+      )
+    });
+
+    return (
+      <footer>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm-5'>
+              <p>© 2017 George Kihara.</p>
+            </div>
+            <div className='col-sm-7 hidden-xs'>
+              <h3 className='lead'>Node_with_react</h3>
+              <ul className='list-inline'>
+                {leaderboardCharacters}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
